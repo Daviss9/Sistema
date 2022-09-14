0668cc4e9005e417659b217f81bf3f04d8bb7b35
Daviss <daviss9@gmail.com>
2022-09-13 22:37:15 -0500

    Ingresos (67): Listar y Busqueda OK

---
+++ Sistema.Datos/DIngreso.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Data;
+using Sistema.Entidades;
+
+namespace Sistema.Datos
+{
+    public class DIngreso
+    {
+        public DataTable Listar()
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Instanciamos la conexion
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                //Llamamos el USP Categoria Listar
+                SqlCommand Comando = new SqlCommand("ingreso_listar", SqlCon);
+                //Indicamos que es de tipo SP
+                Comando.CommandType = CommandType.StoredProcedure;
+                //Abrimos la Conexion
+                SqlCon.Open();
+                //Ejecutamos el SP
+                Resultado = Comando.ExecuteReader();
+                //Cargamos la respuesta a la variable Tabla
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+        public DataTable Buscar(string Valor)
+        {
+            SqlDataReader Resultado;
+            DataTable Tabla = new DataTable();
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Instanciamos la conexion
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                //Llamamos el USP Categoria Listar
+                SqlCommand Comando = new SqlCommand("ingreso_buscar", SqlCon);
+                //Indicamos que es de tipo SP
+                Comando.CommandType = CommandType.StoredProcedure;
+                //Indicamos que el SP espera parametros para realizar la consulta, tipo Varchar, Este value llega de la capa Negocios
+                Comando.Parameters.Add("@valor", SqlDbType.VarChar).Value = Valor;
+                //Abrimos la Conexion
+                SqlCon.Open();
+                //Ejecutamos el SP
+                Resultado = Comando.ExecuteReader();
+                //Cargamos la respuesta a la variable Tabla
+                Tabla.Load(Resultado);
+                return Tabla;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+        }
+        public string Insertar(Ingreso Obj)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("ingreso_insertar", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                //Indicamos que el SP espera parametros Nombre y Descripcion para realizar la consulta, tipo Varchar, Este value llega de la capa Negocios
+                Comando.Parameters.Add("@idproveedor", SqlDbType.Int).Value = Obj.IdProveedor;
+                Comando.Parameters.Add("@idusuario", SqlDbType.Int).Value = Obj.IdUsuario;
+                Comando.Parameters.Add("@tipo_comprobante", SqlDbType.VarChar).Value = Obj.TipoComprobante;
+                Comando.Parameters.Add("@serie_comprobante", SqlDbType.VarChar).Value = Obj.SerieComprobante;
+                Comando.Parameters.Add("@num_comprobante", SqlDbType.VarChar).Value = Obj.NumComprobante;
+                Comando.Parameters.Add("@impuesto", SqlDbType.Decimal).Value = Obj.Impuesto;
+                Comando.Parameters.Add("@total", SqlDbType.Decimal).Value = Obj.Total;
+                Comando.Parameters.Add("@detalle", SqlDbType.Structured).Value = Obj.Detalles;
+                //Abrimos la Conexion
+                SqlCon.Open();
+                //Ejecutamos el SP, usamos el If Ternario, para indicar si se guardo o no
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo guardar el Registro";
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
+
+        }
+
+        public string Anular(int Id)
+        {
+            string Rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon = Conexion.getInstancia().CrearConexion();
+                SqlCommand Comando = new SqlCommand("ingreso_anular", SqlCon);
+                Comando.CommandType = CommandType.StoredProcedure;
+                //Indicamos que el SP espera parametros Nombre y Descripcion para realizar la consulta, tipo Varchar, Este value llega de la capa Negocios
+                Comando.Parameters.Add("@idingreso", SqlDbType.Int).Value = Id;
+                //Abrimos la Conexion
+                SqlCon.Open();
+                //Ejecutamos el SP, usamos el If Ternario, para indicar si se guardo o no
+                Rpta = Comando.ExecuteNonQuery() == 1 ? "OK" : "No se pudo Anular el registro";
+            }
+            catch (Exception ex)
+            {
+                Rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return Rpta;
+        }
+    }
+}
---
+++ Sistema.Presentacion/FrmIngreso.cs
@@ -0,0 +1,100 @@
+﻿using Sistema.Negocio;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Sistema.Presentacion
+{
+    public partial class FrmIngreso : Form
+    {
+        public FrmIngreso()
+        {
+            InitializeComponent();
+        }
+        private void Listar()
+        {
+            try
+            {
+
+                dgvListado.DataSource = NIngreso.Listar();
+                this.Formato();
+                this.Limpiar();
+                lblTotal.Text = "Total Registros: " + Convert.ToString(dgvListado.Rows.Count);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+        private void Buscar()
+        {
+            try
+            {
+
+                dgvListado.DataSource = NIngreso.Buscar(txtBuscar.Text);
+                this.Formato();
+                lblTotal.Text = "Total Registros: " + Convert.ToString(dgvListado.Rows.Count);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void Limpiar()
+        {
+            txtBuscar.Clear();
+            txtDescripcion.Clear();
+            txtNombre.Clear();
+            txtId.Clear();
+
+            btnInsertar.Visible = true;
+            errorIcono.Clear();
+
+            dgvListado.Columns[0].Visible = false;
+            btnAnular.Visible = false;
+            chkSeleccionar.Checked = false;
+        }
+        private void MensajeError(string Mensaje)
+        {
+            MessageBox.Show(Mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+        private void MensajeOk(string Mensaje)
+        {
+            MessageBox.Show(Mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void Formato()
+        {
+            dgvListado.Columns[0].Visible = false;
+            dgvListado.Columns[1].Visible = false;
+            dgvListado.Columns[2].Visible = false;
+            dgvListado.Columns[0].Width = 100;
+            dgvListado.Columns[3].Width = 150;
+            dgvListado.Columns[4].Width = 150;
+            dgvListado.Columns[5].Width = 100;
+            dgvListado.Columns[5].HeaderText = "Documento";
+            dgvListado.Columns[6].Width = 70;
+            dgvListado.Columns[6].HeaderText = "Serie";
+            dgvListado.Columns[7].Width = 70;
+            dgvListado.Columns[7].HeaderText = "Numero";
+            dgvListado.Columns[8].Width = 60;
+            dgvListado.Columns[9].Width = 100;
+            dgvListado.Columns[10].Width = 100;
+            dgvListado.Columns[11].Width = 100;
+        }
+        private void FrmIngreso_Load(object sender, EventArgs e)
+        {
+            this.Listar();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            this.Buscar();
+        }
+    }
+}
